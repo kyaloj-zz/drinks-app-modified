ac391e872d627fc98b603aba6a64023bfcf4462d
owuor91 <owuor91@gmail.com>
2015-05-07 13:35:18 +0300

    Splash and Login activity done

---
+++ app/src/main/java/com/example/teeshirt/drinksapp/Login.java
@@ -0,0 +1,161 @@
+package com.example.teeshirt.drinksapp;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+public class Login extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private AccessTokenTracker myTokenTracker;
+        private ProfileTracker myProfileTracker;
+        private TextView textView;
+        private CallbackManager cbm;
+        private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                displayWelcome(profile);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        };
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+            cbm = CallbackManager.Factory.create();
+            myTokenTracker = new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken old, AccessToken newToken) {
+
+                }
+            };
+
+            myProfileTracker = new ProfileTracker() {
+                @Override
+                protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                    displayWelcome(newProfile);
+                }
+            };
+            myTokenTracker.startTracking();
+            myProfileTracker.startTracking();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            LoginButton loginButton = (LoginButton)view.findViewById(R.id.login_button);
+            loginButton.setReadPermissions("email");
+            loginButton.setFragment(this);
+            loginButton.registerCallback(cbm,mCallback);
+            textView= (TextView)view.findViewById(R.id.text_details);
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            cbm.onActivityResult(requestCode,resultCode,data);
+        }
+
+        private void displayWelcome(Profile profile){
+
+            if (profile != null){
+                textView.setText("Welcome "+ profile.getName());
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Profile profile = Profile.getCurrentProfile();
+            displayWelcome(profile);
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            myTokenTracker.stopTracking();
+            myProfileTracker.stopTracking();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/teeshirt/drinksapp/splash.java
@@ -0,0 +1,35 @@
+package com.example.teeshirt.drinksapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class splash extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splash = new Thread(){
+            public void run(){
+                try {
+                   sleep(5000);
+                    Intent intent = new Intent(splash.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        splash.start();
+    }
+
+
+
+}
